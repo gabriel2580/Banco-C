da6f2390a723b9ffa1bc3668df04b8d3c8a24673
Gabriel <ramosgabriel.188@gmail.com>
2019-09-18 19:42:19 -0300

    Primeiro commit

---
+++ Banco/Dominio/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Dominio
+{
+   public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Nome: {Nome}");
+            sb.AppendLine($"Cpf: {Cpf}");
+            sb.AppendLine($"Data de Nascimento: {DataDeNascimento}");
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Banco/Dominio/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Dominio
+{
+    public class Conta
+    {
+        public StringBuilder Extrato = new StringBuilder();
+
+
+        public string NumeroDaConta { get; set; }
+        public string DigitoVerificador { get; set; }
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public  double DepositarValor {get; set;}
+        public double SacarValor { get; set; }
+        
+        public double Deposita ()
+        {
+           return this.Saldo += DepositarValor;
+        }
+
+        public double Saca()
+        {
+           return this.Saldo -= SacarValor;
+        }
+        
+
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Numero da Conta: { NumeroDaConta}");
+            sb.AppendLine($"Digito Verificador: { DigitoVerificador}");
+            sb.AppendLine($"Saldo: {Saldo.ToString("###,###,###.00")}");
+            sb.AppendLine($"Limite:`{Limite.ToString("###,###,###.00")}");
+            return sb.ToString()
+        }
+
+
+    }
+}
